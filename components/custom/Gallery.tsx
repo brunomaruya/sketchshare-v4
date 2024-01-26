b0a6c46f121f10b50bbb150a01d63aff4a69bfef
Bruno Maruya <brun0yuj1ym02@gmail.com>
2024-01-26 12:23:11 +0900

    Fix bug of Lightbox
    when using Inline plugin, you cant use index property together

--- components/custom/Gallery.tsx
+++ components/custom/Gallery.tsx
@@ -5,6 +5,13 @@ import Masonry from "react-masonry-css";
 import { breakpointColumnsObj } from "../constants/masonry/breakpointColumnsObj";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
+import Inline from "yet-another-react-lightbox/plugins/inline";
+import Captions from "yet-another-react-lightbox/plugins/captions";
+import "yet-another-react-lightbox/plugins/captions.css";
+import Counter from "yet-another-react-lightbox/plugins/counter";
+import "yet-another-react-lightbox/plugins/counter.css";
+
+import "yet-another-react-lightbox/plugins/thumbnails.css";
 
 export default function Gallery({ posts }: { posts: any }) {
   const [index, setIndex] = useState(-1);
@@ -14,10 +21,21 @@ export default function Gallery({ posts }: { posts: any }) {
   async function createUrlsArray() {
     (await posts)
       ? posts.map((post: any) =>
-          setUrls((oldUrls: any) => [...oldUrls, { src: post.imageUrl }])
+          setUrls((oldUrls: any) => [
+            ...oldUrls,
+            { src: post.imageUrl, title: "slide", description: "description" },
+          ])
         )
       : "";
   }
+  const inline = {
+    style: {
+      width: "100%",
+      maxWidth: "900px",
+      aspectRatio: "3 / 2",
+      margin: "0 auto",
+    },
+  };
 
   useEffect(() => {
     createUrlsArray();
@@ -48,16 +66,18 @@ export default function Gallery({ posts }: { posts: any }) {
     <>
       {urls !== undefined ? (
         <Lightbox
-          index={index}
+          // index={index}
           slides={urls}
-          open={index >= 0}
-          close={() => setIndex(-1)}
+          // open={index >= 0}
+          // close={() => setIndex(-1)}
+          inline={inline}
+          plugins={[Inline]}
         />
       ) : (
         ""
       )}
 
-      {posts ? (
+      {/* {posts ? (
         <Masonry
           breakpointCols={breakpointColumnsObj}
           className="my-masonry-grid m-default"
@@ -69,7 +89,7 @@ export default function Gallery({ posts }: { posts: any }) {
         <div>
           <CircularProgress />
         </div>
-      )}
+      )} */}
     </>
   );
 }
