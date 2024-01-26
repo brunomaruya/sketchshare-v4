f2aae639d8f04ace3c0037d26f60b61be06634a0
Bruno Maruya <brun0yuj1ym02@gmail.com>
2024-01-26 11:48:25 +0900

    Added lightbox functionality to the Gallery component
    Added a lightbox component to the Gallery component that allows users to view images in a larger size when clicked on. The lightbox displays the images in a slideshow format.

--- components/custom/Gallery.tsx
+++ components/custom/Gallery.tsx
@@ -1,15 +1,38 @@
 import { CircularProgress } from "@mui/material";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Masonry from "react-masonry-css";
 import { breakpointColumnsObj } from "../constants/masonry/breakpointColumnsObj";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 
 export default function Gallery({ posts }: { posts: any }) {
+  const [index, setIndex] = useState(-1);
+
+  const [urls, setUrls] = useState<any>([]);
+
+  async function createUrlsArray() {
+    (await posts)
+      ? posts.map((post: any) =>
+          setUrls((oldUrls: any) => [...oldUrls, { src: post.imageUrl }])
+        )
+      : "";
+  }
+
+  useEffect(() => {
+    createUrlsArray();
+  }, [posts]);
+
   const gallery = posts
     ? posts.map((post: any, index: any) => {
         return (
           <div className="rounded-md relative" key={index}>
             <Image
+              onClick={() => {
+                urls[index] !== undefined
+                  ? setIndex(index)
+                  : console.log("err");
+              }}
               className="rounded-md"
               src={post.imageUrl}
               width={500}
@@ -23,6 +46,17 @@ export default function Gallery({ posts }: { posts: any }) {
 
   return (
     <>
+      {urls !== undefined ? (
+        <Lightbox
+          index={index}
+          slides={urls}
+          open={index >= 0}
+          close={() => setIndex(-1)}
+        />
+      ) : (
+        ""
+      )}
+
       {posts ? (
         <Masonry
           breakpointCols={breakpointColumnsObj}
