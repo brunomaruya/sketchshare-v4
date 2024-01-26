d0a53353897ec25bf6f5e520463d8be6fc3856a1
Bruno Maruya <brun0yuj1ym02@gmail.com>
2024-01-26 13:16:18 +0900

    Added captions plugin to CarouselComp
    Added the "Captions" plugin to the CarouselComp component in order to display captions for the images in the lightbox.

--- components/custom/CarouselComp.tsx
+++ components/custom/CarouselComp.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import Lightbox from "yet-another-react-lightbox";
 import Inline from "yet-another-react-lightbox/plugins/inline";
+import Captions from "yet-another-react-lightbox/plugins/captions";
 import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/captions.css";
 
 export default function CarouselComp({
   posts,
@@ -11,7 +13,6 @@ export default function CarouselComp({
   index: number;
 }) {
   const [urls, setUrls] = useState<any>([]);
-
   async function createUrlsArray() {
     (await posts)
       ? posts.map((post: any) =>
@@ -26,6 +27,10 @@ export default function CarouselComp({
         )
       : "";
   }
+  useEffect(() => {
+    createUrlsArray();
+  }, [posts]);
+
   const inline = {
     style: {
       width: "100%",
@@ -34,20 +39,50 @@ export default function CarouselComp({
       margin: "0 auto",
     },
   };
+  const [open, setOpen] = React.useState(false);
+  const [index2, setIndex2] = React.useState(0);
 
-  useEffect(() => {
-    createUrlsArray();
-  }, [posts]);
+  const toggleOpen = (state: boolean) => () => setOpen(state);
+
+  const updateIndex = ({ index: current }: { index: number }) =>
+    setIndex2(current);
 
   return (
     <div>
       {index > -1 ? (
-        <Lightbox
-          index={index}
-          slides={urls}
-          inline={inline}
-          plugins={[Inline]}
-        />
+        <>
+          <Lightbox
+            index={index}
+            slides={urls}
+            plugins={[Inline, Captions]}
+            on={{
+              view: updateIndex,
+              click: toggleOpen(true),
+            }}
+            carousel={{
+              padding: 0,
+              spacing: 0,
+              imageFit: "cover",
+            }}
+            inline={{
+              style: {
+                width: "100%",
+                maxWidth: "900px",
+                aspectRatio: "3 / 2",
+                margin: "0 auto",
+              },
+            }}
+          />
+          <Lightbox
+            open={open}
+            close={toggleOpen(false)}
+            index={index2}
+            slides={urls}
+            on={{ view: updateIndex }}
+            animation={{ fade: 0 }}
+            controller={{ closeOnPullDown: true, closeOnBackdropClick: true }}
+          />
+        </>
       ) : (
         ""
       )}
