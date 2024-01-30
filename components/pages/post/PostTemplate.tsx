da3b9d5bf41c8f7cb5f09d6b4a9ec3c3a04d28e6
Bruno Maruya <brun0yuj1ym02@gmail.com>
2024-01-30 11:42:52 +0900

    Refactored useEffect hooks for defining postId, post, and position
    Refactored the useEffect hooks in the PostTemplate component to separate the logic for defining the postId, post, and position into separate functions. This improves readability and maintainability of the code.

--- components/pages/post/PostTemplate.tsx
+++ components/pages/post/PostTemplate.tsx
@@ -5,31 +5,33 @@ import CarouselComp from "../../custom/CarouselComp";
 import { SelectImageContext } from "../../../context/SelectedImageContext";
 
 export default function PostTemplate() {
-  const { imageIndex, setImageIndex } = useContext(SelectImageContext);
   const { posts } = useContext(PostsContext);
   const pathName = window.location.pathname;
   const [postId, setPostId] = useState<String>();
   const [post, setPost] = useState<any>();
   const [position, setPosition] = useState<any>();
 
-  useEffect(() => {
+  const definePostId = () => {
     const postPath = pathName.split("/")[2];
     setPostId(postPath);
+  };
+  const definePost = () => {
+    const pagePost = posts.find((post: any) => post.$id === postId);
+    setPost(pagePost);
+  };
+  const definePosition = () => {
+    const index = posts.findIndex((post: any) => post.$id === postId);
+    setPosition(index);
+  };
+
+  useEffect(() => {
+    definePostId();
   }, []);
 
   useEffect(() => {
     if (posts !== undefined) {
-      const pagePost = posts.find((post: any) => post.$id === postId);
-      console.log(posts);
-      setPost(pagePost);
-    }
-  }, [posts]);
-
-  useEffect(() => {
-    if (posts !== undefined) {
-      const index = posts.findIndex((post: any) => post.$id === postId);
-      setPosition(index);
-      console.log(index);
+      definePost();
+      definePosition();
     }
   }, [posts]);
 
